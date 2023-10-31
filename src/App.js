63c14c95e071e3291f683cd68ea190e0aa5ecc5b
awaghat99 <awaghat99@gmail.com>
2023-10-31 11:52:17 +0000

    make splash layout more responsive

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Splash from "./components/Splash/Splash";
+import About from "./components/About/About";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Splash />
+      <About />
     </div>
   );
 }
---
+++ src/components/About/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./About.css";
+import codeImage from "../../images/code-image.avif";
+
+const About = () => {
+  return <div>{/* <img src={codeImage} alt="computer with code" /> */}</div>;
+};
+
+export default About;
